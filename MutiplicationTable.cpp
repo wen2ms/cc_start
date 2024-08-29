e0c970f614b1b774e4defebf844afb9aa0796264
wen_ms <wen_maosheng@qq.com>
2024-08-29 12:32:42 +0800

    mutiplication table

---
+++ MutiplicationTable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(void) {
+    std::cout << "-----------------------------------\n";
+    std::cout << "I generated the mutiplication table.\n";
+
+    for (int i = 1; i < 10; i ++) {
+        for (int j = 1; j <= i; j ++) {
+            std::cout << j << '*' << i << '=' << j*i << ' ';
+        }
+        std::cout << '\n';
+    }
+    std::cout << "-----------------------------------\n";
+
+    return 0;
+}
