f095cf9d48c47c17fd070b82ce4e8608a513c2c3
wen_ms <wen_maosheng@qq.com>
2024-09-02 11:24:40 +0800

    Array Reverse and Max Elemnt

---
+++ ArrayReverse.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define N 4
+
+int main(void) {
+    int arr[] = {1, 2, 3, 4};
+    int reverse_arr[N];
+
+    std::cout << "--------------------------\n";
+    std::cout << "I generated a reverse array.\n";
+
+    for (int i = 0; i < N/2; i ++) {
+        reverse_arr[i] = arr[N - i - 1];
+        reverse_arr[N - i - 1] = arr[i];
+    }
+    if (N%2 == 1)
+        reverse_arr[N/2] = arr[N/2];
+
+    for (int i = 0; i < N; i++) 
+        std::cout << reverse_arr[i] << ' ';
+
+    std::cout << "\n--------------------------\n";
+
+    return 0;
+}
---
+++ MaxElement.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+template<class ForwardIterator>
+ForwardIterator max_element(ForwardIterator first, ForwardIterator last) {
+    if (first ==last) return first;
+
+    ForwardIterator largest = first;
+
+    first ++;
+    for (; first < last; first ++) {
+        if (*largest < *first)
+            largest = first;
+    } 
+
+    return largest;
+}
+
+int main(void) {
+    std::cout << "------------------------------\n";
+    std::cout << "I generated the maximum element in array.\n";
+
+    int arr[] = {1, 2, 3, 4, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    std::cout << "The maximum element is: " << *max_element(arr, arr + n) << '\n';
+    std::cout << "------------------------------\n";
+
+
+    return 0;
+}
