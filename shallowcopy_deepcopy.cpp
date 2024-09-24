f46506c42bc9fb83c91871dcba44bc8fd9b4a8b9
wen_ms <wen_maosheng@qq.com>
2024-09-24 18:21:41 +0800

    feat: shallowcopy and deepcopy

---
+++ shallowcopy_deepcopy.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cstring>
+
+class ShallowCopy {
+  public:
+    char* data;
+
+    ShallowCopy(const char str[]) {
+        data = new char[strlen(str) + 1];
+        strcpy(data, str);
+    }
+
+    ShallowCopy(const ShallowCopy& other) {
+        data = other.data;
+    }
+
+    ~ShallowCopy() {
+        delete[] data;
+    }
+};
+
+class DeepCopy {
+  public:
+    char* data;
+
+    DeepCopy(const char str[]) {
+        data = new char[strlen(str) + 1];
+        strcpy(data, str);
+    }
+
+    DeepCopy(const DeepCopy& other) {
+        data = new char[strlen(other.data) + 1];
+        strcpy(data, other.data);
+    }
+
+    ~DeepCopy() {
+        delete[] data;
+    }
+};
+
+int main() {
+    std::cout << "I generated an example that explains shallowcopy and deepcopy\n";
+    std::cout << "----------------------\n";
+
+    ShallowCopy shallowcopy_a("Dobermann");
+    ShallowCopy shallowcopy_b = shallowcopy_a;
+
+    std::cout << "Shallow copy data: " << shallowcopy_a.data << " " << shallowcopy_b.data << '\n';
+
+    shallowcopy_b.data[0] = 'M';
+    std::cout << "Modified shallow copy data: " << shallowcopy_a.data << " " << shallowcopy_b.data << '\n';
+
+    DeepCopy deepcopy_a("French Bulldog");
+    DeepCopy deepcopy_b = deepcopy_a;
+
+    std::cout << "DeepCopy copy data: " << deepcopy_a.data << " " << deepcopy_b.data << '\n';
+
+    deepcopy_b.data[0] = 'M';
+    std::cout << "Modified deepCopy copy data: " << deepcopy_a.data << " " << deepcopy_b.data << '\n';
+
+    std::cout << "----------------------\n";
+
+    return 0;
+}
