abe9586d358dab9a50f0f45b6ce4b5683d9fca8a
wen_ms <wen_maosheng@qq.com>
2024-10-02 22:21:44 +0800

    feat: virtual inheritance

---
+++ virtual_inheritance.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+class Animal {
+  public:
+    static int age_;
+};
+
+class Sheep : virtual public Animal {
+};
+
+class Camel : virtual public Animal {
+};
+
+class Llama : public Sheep, public Camel {
+};
+
+int Animal::age_ = 0;
+
+int main() {
+    std::cout << "I have gererated a virtual inheritance.\n";
+    std::cout << "------------------------------\n";
+
+    std::cout << "Animal age: " << Llama::age_ << '\n';
+
+    std::cout << "------------------------------\n";
+
+    return 0;
+}
