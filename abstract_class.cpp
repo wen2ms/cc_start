72e521b82facf8f110893c6030e028a509a27560
wen_ms <wen_maosheng@qq.com>
2024-10-15 17:49:23 +0800

    feat: abstract class

---
+++ abstract_class.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cmath>
+
+class Shape {
+  public:
+    virtual double cal_area() const = 0;
+
+    virtual ~Shape() {}
+};
+
+class Rectangle : public Shape {
+  public:
+    Rectangle(double width, double height) : width_(width), height_(height) {};
+
+    double cal_area() const override {
+        return width_ * height_;
+    }
+
+    double width_, height_;
+};
+
+class Circle : public Shape {
+  public:
+    Circle(double radius) : radius_(radius) {}
+
+    double cal_area() const override {
+        return M_PI * radius_ * radius_;
+    }
+
+  private:
+    double radius_;
+};
+
+int main() {
+    std::cout << "I generated a abstract class\n";
+    std::cout << "---------------------------\n";
+    
+    Shape* rectangle = new Rectangle(3.0, 4.0);
+    Shape* circle = new Circle(3.0);
+
+    std::cout << "Area: " << rectangle->cal_area() << '\n';
+    std::cout << "Area: " << circle->cal_area() << '\n';
+    
+    delete rectangle;
+    delete circle;
+
+    std::cout << "---------------------------\n";
+
+    return 0;
+}
