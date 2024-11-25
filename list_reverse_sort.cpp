ea07c302ac2ae32f1c1d026af77f137c22b8231d
wen2ms <1819509111@qq.com>
2024-11-25 16:04:03 +0800

    feat: advanced list sorting

--- list_reverse_sort.cpp
+++ list_reverse_sort.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-#include <string>
 #include <list>
+#include <string>
 
 class TVShow {
   public:
-    TVShow(std::string name = "Unknown", float ranking = -1): name_(name), ranking_(ranking) {}
+    TVShow(std::string name = "Unknown", float rating = -1, int realase_date = 0)
+        : name_(name), rating_(rating), release_date_(realase_date) {}
 
     std::string name_;
-    float ranking_;
+    float rating_;
+    int release_date_;
 };
 
 void print_list(const std::list<TVShow>& shows_list) {
     for (std::list<TVShow>::const_iterator it = shows_list.begin(); it != shows_list.end(); it++) {
-        std::cout << it->name_ << ' ' << it->ranking_ << "  ";
+        std::cout << it->name_ << ' ' << it->rating_ << "  ";
     }
     std::cout << std::endl;
 }
@@ -23,17 +25,21 @@ int main() {
 
     std::list<TVShow> shows_list;
 
-    shows_list.push_back(TVShow("Game of Thrones", 9.3));
-    shows_list.push_back(TVShow("Sherlock", 9.1));
-    shows_list.push_back(TVShow("Friends", 8.9));
-    shows_list.push_back(TVShow("Frieren", 8.8));
-    shows_list.push_back(TVShow("Arcane", 8.5));
+    shows_list.push_back(TVShow("Game of Thrones", 9.3, 2011));
+    shows_list.push_back(TVShow("Sherlock", 9.1, 2010));
+    shows_list.push_back(TVShow("Friends", 8.9, 1994));
+    shows_list.push_back(TVShow("Frieren", 8.9, 2023));
+    shows_list.push_back(TVShow("Arcane", 9.0, 2021));
 
     std::cout << "Before sorting: " << std::endl;
     print_list(shows_list);
 
     shows_list.sort([](const TVShow& a, const TVShow& b) {
-        return a.ranking_ < b.ranking_;
+        if (a.rating_ == b.rating_) {
+            return a.release_date_ < b.release_date_;
+        } 
+
+        return a.rating_ < b.rating_;
     });
 
     std::cout << "After sorting: " << std::endl;
