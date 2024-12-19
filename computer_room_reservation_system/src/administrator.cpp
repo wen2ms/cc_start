4379d48b0c588cc21dc014329707467316083014
wen2ms <1819509111@qq.com>
2024-12-20 00:40:30 +0800

    feat: administrator adds account without checking for duplicates

--- computer_room_reservation_system/src/administrator.cpp
+++ computer_room_reservation_system/src/administrator.cpp
@@ -2,8 +2,10 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <fstream>
 
 #include "utilities.h"
+#include "crr_system_config.h"
 
 Administrator::Administrator(std::string name, std::string password) : Identity(name, password) {}
 
@@ -63,7 +65,51 @@ void Administrator::show_title() {
 }
 
 void Administrator::add_account() {
+    std::cout << "Enter the type of account to add" << std::endl;
+    std::cout << "1. student" << std::endl;
+    std::cout << "2. teacher" << std::endl;
 
+    std::string key;
+    std::cin >> key;
+
+    if (key != "1" && key != "2") {
+        std::cout << "Number invalid, please enter again..." << std::endl;
+        return;
+    }
+
+    std::string input_name;
+    std::string input_id;
+    std::string input_password;
+
+    std::string filename;
+
+    if (key == "1") {
+        std::cout << "Please input your student id: ";
+        filename = STUDENT_DIR;
+    } else if (key == "2") {
+        std::cout << "Please input your teacher id: ";
+        filename = TEACHER_DIR;
+    }
+    std::cin >> input_id;
+
+    std::cin.ignore();
+    std::cout << "Please input your name: ";
+    std::getline(std::cin, input_name);
+
+    std::cout << "Please input your password: ";
+    std::cin >> input_password;
+
+    std::ofstream outfile(filename, std::ios_base::app);
+    if (!outfile.is_open()) {
+        std::cout << "Could not open " << '\'' << filename << '\''  << " for writing" << std::endl;
+        return;
+    }
+
+    outfile << input_id << ',' << input_name << ',' << input_password << std::endl;
+
+    std::cout << "Add successfully!" << std::endl;
+
+    outfile.close();
 }
 
 void Administrator::view_all_accounts() {
