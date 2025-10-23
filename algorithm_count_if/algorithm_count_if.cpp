d129cd6119655ac7716e868e32bb2708046c977b
wen2ms <1819509111@qq.com>
2025-10-23 13:34:43 +0800

    Feat: std::count and std::count_if

--- algorithm_count_if/algorithm_count_if.cpp
+++ algorithm_count_if/algorithm_count_if.cpp
@@ -1,12 +1,12 @@
 #include <algorithm>
+#include <fstream>
 #include <iostream>
 #include <list>
 #include <string>
-#include <fstream>
 
 class App {
   public:
-    App(std::string app_name = "", int rank = -1) : app_name_(app_name), rank_(rank) {}
+    explicit App(std::string app_name = "", int rank = -1) : app_name_(app_name), rank_(rank) {}
 
     bool operator==(const App& other_app) const {
         return app_name_ == other_app.app_name_;
@@ -18,14 +18,10 @@ class App {
 
 class CountTargetCompany {
   public:
-    CountTargetCompany(std::string target_company) : target_company_(target_company) {}
+    explicit CountTargetCompany(const std::string& target_company) : target_company_(target_company) {}
 
     bool operator()(const App& app) const {
-        if (app.app_name_.find(target_company_) == std::string::npos) {
-            return false;
-        } else {
-            return true;
-        }
+        return app.app_name_.find(target_company_) != std::string::npos;
     }
 
   private:
@@ -34,11 +30,11 @@ class CountTargetCompany {
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
-        std::cout << "Usage: " << argv[0] << "Filename TargetAppName" << std::endl;
+        std::cout << "Usage: " << argv[0] << "Filename TargetAppName" << '\n';
         return 1;
     }
 
-    std::cout << "I generated an example of stl count_if" << std::endl;
+    std::cout << "I generated an example of stl count_if" << '\n';
     std::cout << "------------------------------\n";
 
     const std::string kFileName = argv[1];
@@ -46,7 +42,7 @@ int main(int argc, char* argv[]) {
     std::ifstream infile(kFileName);
 
     if (!infile.is_open()) {
-        std::cout << "Could not open " << kFileName << " for reading" << std::endl;
+        std::cout << "Could not open " << kFileName << " for reading" << '\n';
         return 2;
     }
 
@@ -58,20 +54,20 @@ int main(int argc, char* argv[]) {
 
         std::string app_name = line.substr(0, comma_position);
         int rank = std::stoi(line.substr(comma_position + 1));
-        
+
         input_list.push_back(App(app_name, rank));
     }
 
-    for (std::list<App>::iterator it = input_list.begin(); it != input_list.end(); it++) {
-        std::cout << it->app_name_ << ' ' << it->rank_ << std::endl;
+    for (const auto& [app_name, rank] : input_list) {
+        std::cout << app_name << ' ' << rank << '\n';
     }
 
     const std::string kTargetAppName = argv[2];
     int target_counts = std::count(input_list.begin(), input_list.end(), App(kTargetAppName));
     int target_counts_if = std::count_if(input_list.begin(), input_list.end(), CountTargetCompany(kTargetAppName));
 
-    std::cout << kTargetAppName << " Counts: " << target_counts << std::endl;
-    std::cout << kTargetAppName << " CountsIf: " << target_counts_if << std::endl;
+    std::cout << kTargetAppName << " Counts: " << target_counts << '\n';
+    std::cout << kTargetAppName << " CountsIf: " << target_counts_if << '\n';
 
     std::cout << "------------------------------\n";
 
--- virtual_inheritance.cpp
+++ virtual_inheritance.cpp
@@ -2,14 +2,27 @@
 
 class Animal {
   public:
+    explicit Animal(int num) {
+        std::cout << "num = " << num << '\n';
+    }
+
     static int age_;
 };
 
-class Sheep : virtual public Animal {};
+class Sheep : virtual public Animal {
+  public:
+    Sheep() : Animal(1) {}
+};
 
-class Camel : virtual public Animal {};
+class Camel : virtual public Animal {
+  public:
+    Camel() : Animal(2) {}
+};
 
-class Llama : public Sheep, public Camel {};
+class Llama : public Sheep, public Camel {
+  public:
+    Llama() : Sheep(), Camel(), Animal(3) {}
+};
 
 int Animal::age_ = 0;
 
@@ -17,6 +30,7 @@ int main() {
     std::cout << "I have gererated a virtual inheritance.\n";
     std::cout << "------------------------------\n";
 
+    Llama llama;
     std::cout << "Animal age: " << Llama::age_ << '\n';
 
     std::cout << "------------------------------\n";
